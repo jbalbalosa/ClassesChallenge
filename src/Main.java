ec4c31e9fa616e4ae850e8b8ffa5569ebb89fc9a
Jeffrey Balbalosa <31784615+jbalbalosa@users.noreply.github.com>
2024-09-02 10:56:58 +0900

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+                BankAccount ba = new BankAccount();
+
+        ba.setAccountNumber("123456");
+        ba.setAccountBalance(200);
+        ba.setCustomerName("Jeff");
+        ba.setPhoneNumber("680-7782572");
+        ba.setEmail("jeffrey@surangel.com");
+
+        ba.displayAccountDetails();
+//
+//        ba.deposit(20000);
+//        ba.withdraw(50);
+//        ba.withdraw(50);
+//        ba.deposit(75);
+//        ba.deposit(300);
+        ba.withdraw(300);
     }
 }
